1f4d51afd9fe60ffe84ca3a4fde0f697bf2df4f3
zhongrongzhao <30423830+zhongrongzhao@users.noreply.github.com>
2020-04-24 17:01:12 +0800

    Add files via upload

---
+++ 鱼蛋修改系统工具 V1.2/鱼蛋修改系统工具/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace 鱼蛋修改系统工具
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(141, 141);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(10, 175);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 14);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "输入“制造商”名称：";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.Location = new System.Drawing.Point(12, 202);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(141, 23);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(194, 202);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(92, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "修改";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(172, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(155, 141);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = resources.GetString("richTextBox1.Text");
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(194, 172);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(92, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "清除";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(291, 224);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(29, 12);
+            this.linkLabel1.TabIndex = 6;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "赞助";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(326, 242);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "鱼蛋修改系统工具 v1.2（64位）";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ 鱼蛋修改系统工具 V1.2/鱼蛋修改系统工具/Form1.cs
@@ -0,0 +1,229 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Runtime.CompilerServices;
+using System.Net.Http.Headers;
+
+namespace 鱼蛋修改系统工具
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private static string PICPath;
+
+        /// <summary>
+        /// 改注册表
+        /// </summary>
+        private void ModifyRegistry()
+        {
+            try
+            {
+                if (pictureBox1.Image != null)
+                {
+                    using (RegistryKey registryKey = Registry.LocalMachine)
+                    {
+                        RegistryKey software = registryKey.OpenSubKey("SOFTWARE", true);
+                        RegistryKey Microsoft = software.OpenSubKey("Microsoft", true);
+                        RegistryKey Windows = Microsoft.OpenSubKey("Windows", true);
+                        RegistryKey CurrentVersion = Windows.OpenSubKey("CurrentVersion", true);
+                        RegistryKey OEMInformation = CurrentVersion.OpenSubKey("OEMInformation", true);
+                        OEMInformation.SetValue("logo", "C:\\SystemIamge\\oemlogo.bmp", RegistryValueKind.String);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("写入失败原因：" + ex);
+            }
+        }
+
+        /// <summary>
+        /// 改注册表
+        /// </summary>
+        /// <param name="Name">供应商名</param>
+        private void ModifyRegistry(string Name)
+        {
+            try
+            {
+                if (pictureBox1.Image != null)
+                {
+                    using (RegistryKey registryKey = Registry.LocalMachine)
+                    {
+                        RegistryKey software = registryKey.OpenSubKey("SOFTWARE", true);
+                        RegistryKey Microsoft = software.OpenSubKey("Microsoft", true);
+                        RegistryKey Windows = Microsoft.OpenSubKey("Windows", true);
+                        RegistryKey CurrentVersion = Windows.OpenSubKey("CurrentVersion", true);
+                        RegistryKey OEMInformation = CurrentVersion.OpenSubKey("OEMInformation", true);
+                        OEMInformation.SetValue("logo", "C:\\SystemIamge\\oemlogo.bmp", RegistryValueKind.String);
+                    }
+                }
+
+                if (textBox1.Text != null)
+                {
+                    using (RegistryKey registryKey = Registry.LocalMachine)
+                    {
+                        RegistryKey software = registryKey.OpenSubKey("SOFTWARE", true);
+                        RegistryKey Microsoft = software.OpenSubKey("Microsoft", true);
+                        RegistryKey Windows = Microsoft.OpenSubKey("Windows", true);
+                        RegistryKey CurrentVersion = Windows.OpenSubKey("CurrentVersion", true);
+                        RegistryKey OEMInformation = CurrentVersion.OpenSubKey("OEMInformation", true);
+                        OEMInformation.SetValue("Manufacturer", Name, RegistryValueKind.String);
+                    }
+                }
+            }
+            catch(Exception ex) 
+            {
+                MessageBox.Show("写入失败原因：" + ex);
+            }
+        }
+
+        private void PictureTransform(string image,string path)
+        {
+            Transform.MakeSquareImage(image, path, 120);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog openFileDialog1 = new OpenFileDialog();   //显示选择文件对话框 
+                openFileDialog1.InitialDirectory = "c:\\";
+                openFileDialog1.Filter = "bmp files (*.bmp)|*.bmp";
+                openFileDialog1.FilterIndex = 2;
+                openFileDialog1.RestoreDirectory = true;
+
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    PICPath = openFileDialog1.FileName;
+                    //pictureBox1.Image = Image.FromFile(PICPath);
+                    string SubPath = "C:\\SystemIamge\\";
+                    string CopyPath = "C:\\SystemIamge\\oemlogo.bmp";
+                    if (false == Directory.Exists(SubPath))
+                    {
+                        Directory.CreateDirectory(SubPath);
+                        //File.Copy(PICPath, CopyPath);
+                        PictureTransform(PICPath, CopyPath);
+                        pictureBox1.Image = Image.FromFile(CopyPath);
+                    }
+                    else
+                    {
+                        //File.Copy(PICPath, CopyPath,true);
+                        PictureTransform(PICPath, CopyPath);
+                        pictureBox1.Image = Image.FromFile(CopyPath);
+                    }
+                                  
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("插入图片失败，错误原因为：" + ex.Message);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = null;
+            textBox1.Text = null;
+            object ManufacturerValue = Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation", "Manufacturer", null);
+            object LogoValue = Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation", "logo", null);
+            
+            if(ManufacturerValue != null && LogoValue == null)
+            {
+                using (RegistryKey registryKey = Registry.LocalMachine)
+                {
+                    RegistryKey software = registryKey.OpenSubKey("SOFTWARE", true);
+                    RegistryKey Microsoft = software.OpenSubKey("Microsoft", true);
+                    RegistryKey Windows = Microsoft.OpenSubKey("Windows", true);
+                    RegistryKey CurrentVersion = Windows.OpenSubKey("CurrentVersion", true);
+                    RegistryKey OEMInformation = CurrentVersion.OpenSubKey("OEMInformation", true);
+                    OEMInformation.DeleteValue("Manufacturer", true);
+                    MessageBox.Show("清除成功！", "提示");
+                }
+            }
+
+            if(ManufacturerValue == null && LogoValue != null)
+            {
+                using (RegistryKey registryKey = Registry.LocalMachine)
+                {
+                    RegistryKey software = registryKey.OpenSubKey("SOFTWARE", true);
+                    RegistryKey Microsoft = software.OpenSubKey("Microsoft", true);
+                    RegistryKey Windows = Microsoft.OpenSubKey("Windows", true);
+                    RegistryKey CurrentVersion = Windows.OpenSubKey("CurrentVersion", true);
+                    RegistryKey OEMInformation = CurrentVersion.OpenSubKey("OEMInformation", true);
+                    OEMInformation.DeleteValue("logo", true);
+                    MessageBox.Show("清除成功！", "提示");
+                }
+            }
+
+            if (ManufacturerValue != null && LogoValue != null)
+            {
+                using (RegistryKey registryKey = Registry.LocalMachine)
+                {
+                    RegistryKey software = registryKey.OpenSubKey("SOFTWARE", true);
+                    RegistryKey Microsoft = software.OpenSubKey("Microsoft", true);
+                    RegistryKey Windows = Microsoft.OpenSubKey("Windows", true);
+                    RegistryKey CurrentVersion = Windows.OpenSubKey("CurrentVersion", true);
+                    RegistryKey OEMInformation = CurrentVersion.OpenSubKey("OEMInformation", true);
+                    OEMInformation.DeleteValue("Manufacturer", true);
+                    OEMInformation.DeleteValue("logo", true);
+                    MessageBox.Show("清除成功！", "提示");
+                }
+            }
+
+            if (LogoValue == null && ManufacturerValue==null)
+            {
+                MessageBox.Show("没有内容可清空","提示");
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text =="" && pictureBox1.Image != null)
+            {
+                ModifyRegistry();
+                MessageBox.Show("修改成功！", "提示");
+            }
+
+            if(textBox1.Text =="" && pictureBox1.Image == null)
+            {
+                MessageBox.Show("请输入要修改的项目","提示");
+            }
+
+            if(textBox1.Text != "" && pictureBox1.Image == null)
+            {
+                ModifyRegistry(textBox1.Text.ToString());
+                MessageBox.Show("修改成功！", "提示");
+            }       
+
+            if(textBox1.Text != "" && pictureBox1.Image != null)
+            {
+                ModifyRegistry(textBox1.Text.ToString());
+                MessageBox.Show("修改成功！", "提示");
+            }
+        }
+
+        /// <summary>
+        /// 打开赞助页面
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://zhongrongzhao.github.io/support.html");
+        }
+    }
+}
